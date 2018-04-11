26ca92527eae98c563c99193cb2f6a0892a5e8be
unknown <anthoniomaduadichie@gmail.com>
2018-04-11 10:07:56 +0100

    stage 1 HNG4

---
+++ hng.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<link rel="stylesheet" type="text/css" href="hng.css">
+
+<head>
+<link rel="stylesheet" type="text/css" href="hng.css">
+<style>
+body {
+    position: relative;
+    background-image: url("lavender.jpg");
+    background-repeat:no-repeat;
+    background-size:100% 100vh;
+
+}
+h1 {
+	 text-align: center;
+     
+     font-family: Brandon Grotesque;
+     color: white;
+      left: 0;
+     height: 100%;
+  width: 100%;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+ 
+}
+
+</style>
+</head>
+
+<body  >
+<h1>
+<?php 
+  date_default_timezone_set("Africa/Lagos");
+echo "Time " . date("h:ia");
+  ?>
+  <br>HNGinternship4
+  <h1>
+  
+ 
+</body>
+
+</html>
